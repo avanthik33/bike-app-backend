363c7c032b43cc8ef8c35c55feefbb6f9127d0c7
avanthik33 <avanthik33@gmail.com>
2024-02-08 21:59:24 +0530

    .\.git\

--- routers/bikeRouter.js
+++ routers/bikeRouter.js
@@ -5,8 +5,14 @@ const router=express.Router()
 
 router.post("/add",async(req,res)=>{
   let data=req.body
-  let bikeModelObj1=await new bikeModel(data)
-  res.json(bikeModelObj1)
+  let bikeModelObj1=new bikeModel(data)
+  await bikeModelObj1.save()
+  res.json({status:"success"})
+})
+
+router.get("/view",async(req,res)=>{
+  let data=await bikeModel.find()
+  res.json(data)
 })
 
 module.exports=router
